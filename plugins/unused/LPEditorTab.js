86e2bb247b09c037261388e10578535540b77260
CrungeyDownloadsViruses <76408831+CrungeyDownloadsViruses@users.noreply.github.com>
2026-01-17 15:28:49 -0500

    Add files via upload

---
+++ plugins/unused/LPEditorTab.js
@@ -0,0 +1,61 @@
+export default function({ appendToId, appendToClass, appendToFunctionInScript, appendToFunctionAtMarker }) {
+  // 1️⃣ Add the LuckPerms tab button at the end
+  appendToId('tabs', `
+    <div class="tab" onclick="currentTab = 7; updateTabs();" style="background: #282828; width: 150px; height: 100%; border-radius: 5px 5px 0px 0px; text-align: center; padding-top: 0.03%; display: inline-block; margin-right: 5px;">LuckPerms</div>
+  `);
+
+  // 2️⃣ Add the LuckPerms tab content inside mainContent
+  appendToId('mainContent', `
+    <div class="LuckPerms" style="display:none; margin-left: 15px; margin-right: 15px; padding-top: 5px; border-radius: 0px 5px 0px 0px; background: linear-gradient(180deg, rgba(59, 69, 72, 1) 0%, rgba(22, 22, 22, 1) 100%); min-height: 60px;"></div>
+  `);
+
+
+
+  appendToFunctionInScript(
+    0, // first <script> tag in index.html
+    "waitForLP", // name of your new function
+    `
+        let editormsg = "";
+      function waitloop() {
+          for(let i = 0; i < document.getElementById('console-output').children.length; i++) {
+              if(document.getElementById('console-output').children[i].innerText.includes('https://luckperms.net/editor/')) {
+                  editormsg = 'https://luckperms.net/editor/' + document.getElementById('console-output').children[i].innerText.split('https://luckperms.net/editor/')[1].split('[0m')[0];
+                  document.getElementsByClassName('LuckPerms')[0].innerHTML = "<iframe src='" + editormsg + "' style='width: 100%; height: calc(100vh - 150px); border-radius: 0px 0px 5px 5px;'></iframe>";
+                  //stop the loop
+                  clearInterval(lpinterval);
+              }
+          }
+      }
+      const lpinterval =setInterval(waitloop, 100);
+    `
+  );
+
+  // 3️⃣ Append logic to updateTabs function to handle currentTab == 7
+  appendToFunctionAtMarker(
+    0, // assuming first <script> tag in index.html contains updateTabs
+    "// PLUGIN_INJECTION_POINT-CurrentTabFunctions",
+    `
+    if (currentTab == 7) {
+
+    fetch("/currentInstInfo", {
+    method: 'GET'
+    }).then(res => {
+    return res.json().then(data => {
+        if(data.isOnline == false) {
+            document.getElementsByClassName('LuckPerms')[0].innerText = "Please start the server first.";
+        }
+        else
+        {
+            document.getElementsByClassName('LuckPerms')[0].innerText = "Loading...";
+            sendConsoleInput("lp editor");
+            waitForLP();
+            
+        }
+    });
+    });
+
+      
+    }
+    `
+  );
+}
